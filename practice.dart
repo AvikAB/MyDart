49b5a6b601e9488ca0355dff6ef92b78d8cf4520
Avik <119071924+AvikAB@users.noreply.github.com>
2025-08-04 13:49:13 +0600

    Update practice.dart

--- practice.dart
+++ practice.dart
@@ -1,4 +1,4 @@
-// Datatypes and Variables:
+/// Datatypes and Variables:
 
 void main() {main.dart
   String name = "Avik";
@@ -12,4 +12,29 @@ void main() {main.dart
     "fruits" : fruits,
   };
   print(myData);
+}
+
+/// List:
+// Fixed length list
+void main() {
+  List? li = List.filled(5, null, growable:false);
+  li[0] = 1;
+  li[1] = 2;
+  li[2] = 3;
+  print(li);
+  print(li[1]);
+}
+
+// Growable list
+void main() {
+  var li = [1,2,3];
+  print(li);
+  li.add(4);  // add(ele)
+  print(li);
+  li.addAll([2,6,7,8,9]);   // addAll([ele1, ele2,...])
+  print(li);
+  li.insert(2, 100);   // insert(idx, val)
+  print(li);
+  li.insertAll(4, [200,300]);  //insertAll([ele1, ele2, ...])
+  print(li);
 }
