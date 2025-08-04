a1e2eaf242f55996b57dec4af622c1b7dfeaf84c
Avik <119071924+AvikAB@users.noreply.github.com>
2025-08-04 11:44:00 +0600

    Create practice.dart

---
+++ practice.dart
@@ -0,0 +1,15 @@
+// Datatypes and Variables:
+
+void main() {main.dart
+  String name = "Avik";
+  int age = 23;
+  bool male = true;
+  List<String> fruits = ["Apple", "Mango", "Guava"];
+  Map<String, dynamic> myData = {
+    "name" : name,
+    "age" : age,
+    "male" : male, 
+    "fruits" : fruits,
+  };
+  print(myData);
+}
